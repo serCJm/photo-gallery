8ed455cd88c3b4dec711e6a5c043587668b99fad
CJ Moro <scjmoro@gmail.com>
2018-12-04 06:02:30 -0800

    Add logic for viewing image on a thumbnail click

--- js/gallery-view.js
+++ js/gallery-view.js
@@ -2,20 +2,69 @@
 
 window.onload = function() {
   (function() {
-    const imgs = document.querySelector(".image-gallery");
+    const gallery = document.querySelector(".image-gallery");
 
-    imgs.addEventListener("click", imgClick);
+    const imgs = document.querySelectorAll(".image");
+
+    // used of keeping track of a current image in a slider
+    let thumbnailSrc;
+    let thumbnailIndex;
+    // get src attributes from thumbnails
+    const srcCache = [];
+    for (let i = 0; i < imgs.length; i++) {
+      srcCache.push(imgs[i].getAttribute("src"));
+    }
+
+    gallery.addEventListener("click", imgClick);
 
     function imgClick(e) {
-      const imgModal = document.querySelector("modal-view");
+      const imgModal = document.querySelector(".modal-view");
+      // open image viewer
       if (e.target.classList.contains("image-container")) {
-        const thumbnailSrc = e.target.children[0].src.slice(74);
-        const thumbnailSrcEnd = thumbnailSrc.slice(74);
-
-        imgModal.classList.toggle("show");
+        thumbnailSrc = e.target.children[0].getAttribute("src");
+        thumbnailIndex = srcCache.indexOf(thumbnailSrc);
+        viewImage(thumbnailSrc, imgModal);
+        // close image viewer
       } else if (e.target.classList.contains("modal-view")) {
-        imgModal.classList.toggle("show");
+        closeImage(imgModal);
+        // view images left and right
+      } else if (e.target.classList.contains("arrow")) {
+        const current = imgModal.querySelector(".modal-image");
+        if (e.target.classList.contains("left")) {
+          if (thumbnailIndex === 0) {
+            thumbnailIndex = srcCache.length;
+          } else {
+            thumbnailIndex--;
+          }
+        } else {
+          console.log("right");
+        }
+        const thumbnailSrcEnd = srcCache[thumbnailIndex].slice(27);
+        current.setAttribute(
+          "src",
+          `imgs/large/birth-photo-large-${thumbnailSrcEnd}`
+        );
       }
+    }
+
+    function viewImage(thumbnailSrc, imgModal) {
+      // get the ending of of the thumbnail image src
+      const thumbnailSrcEnd = thumbnailSrc.slice(27);
+      const largeImg = document.createElement("img");
+      imgModal.appendChild(largeImg);
+      largeImg.setAttribute(
+        "src",
+        `imgs/large/birth-photo-large-${thumbnailSrcEnd}`
+      );
+      largeImg.setAttribute("alt", "birth-photo-large");
+      largeImg.classList.add("modal-image");
+      imgModal.classList.toggle("show");
+    }
+
+    function closeImage(imgModal) {
+      imgModal.classList.toggle("show");
+      const largeImg = imgModal.querySelector(".modal-image");
+      imgModal.removeChild(largeImg);
     }
   })();
 };
